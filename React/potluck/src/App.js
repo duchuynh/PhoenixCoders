7b5ded73ab619ea75976aea48525cbc66177e34f
smarinacseas <smarinac@seas.upenn.edu>
2024-09-21 11:27:12 -0400

    Basic routing between login, onboarding, then home pages

--- React/potluck/src/App.js
+++ React/potluck/src/App.js
@@ -1,32 +1,20 @@
-import React, { useState } from 'react';
-import './App.css';
-import MealForm from './MealForm';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login/Login';
+import Onboarding from './pages/Onboarding/Onboarding';
+import Home from './pages/Home/Home';
+
 
 function App() {
-  const [meals, setMeals] = useState([]);
-
-  const generateMeals = async (people) => {
-    try {
-      const response = await fetch(`http://127.0.0.1:8000/meals/?number_of_people=${people}`);
-      const data = await response.json();
-      setMeals(data.meals);
-    } catch (error) {
-      console.error("Error fetching meals:", error);
-    }
-  };
-
   return (
-    <div className="App">
-      <h1>Meal Planner</h1>
-      <MealForm onGenerateMeals={generateMeals} />
-      <ul style={{ listStyleType: 'none', padding: '0' }}>
-        {meals.map((meal, index) => (
-          <li key={index} style={{ margin: '10px 0', padding: '10px', border: '1px solid #ccc', borderRadius: '4px' }}>
-            {meal}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
