68dc46c4da6eafdd4b8e8a3241b4e1975b66050d
smarinacseas <smarinac@seas.upenn.edu>
2024-09-21 19:41:22 -0400

    Login styling (applies to entire web app)

--- React/potluck/src/pages/Login/Login.js
+++ React/potluck/src/pages/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import api from '../../api';
+import './Login.css';
 
 const Login = () => {
   const [firstName, setFirstName] = useState('');
