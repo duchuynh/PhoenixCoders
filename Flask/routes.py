3d68f334660d71fca3b4702f08218d9e509da3ff
smarinacseas <smarinac@seas.upenn.edu>
2024-09-21 13:01:23 -0400

    Commented out, now working

--- Flask/routes.py
+++ Flask/routes.py
@@ -1,17 +1,17 @@
-from flask import request, Blueprint, jsonify
+# from flask import request, Blueprint, jsonify
 
-# Define a blueprint for main routes
-main_routes = Blueprint('main_routes', __name__)
+# # Define a blueprint for main routes
+# main_routes = Blueprint('main_routes', __name__)
 
-@main_routes.route('/api/data', methods=['GET'])
-def get_data():
-    data = {'message': 'Hello from Flask!'}
-    return jsonify(data)
+# @main_routes.route('/api/data', methods=['GET'])
+# def get_data():
+#     data = {'message': 'Hello from Flask!'}
+#     return jsonify(data)
 
-@main_routes.route('/api/data', methods=['POST'])
-def post_data():
-    data = request.json
-    return jsonify({'status': 'success', 'data_received': data})
+# @main_routes.route('/api/data', methods=['POST'])
+# def post_data():
+#     data = request.json
+#     return jsonify({'status': 'success', 'data_received': data})
 
 # Authentication (username/password)
 
